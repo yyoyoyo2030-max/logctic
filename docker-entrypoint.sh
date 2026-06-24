66c01b72172261ed52716a3f4161c5ddd2f4390f
Logistic Admin <admin@logistic.local>
2026-06-24 22:13:28 +0300

    Perf: Enable PDO persistent connections to reduce latency to remote cloud DB

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -87,6 +87,7 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_PERSISTENT => true,
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4, time_zone = '+03:00'"
     ];
     if (DB_SSL) {

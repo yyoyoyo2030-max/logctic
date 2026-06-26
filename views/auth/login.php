156300e9b9f5c572f7f0145cf009af9f0db00845
Logistic Admin <admin@logistic.local>
2026-06-26 04:02:45 +0300

    Temp: Enable display errors for debugging blank page

--- views/auth/login.php
+++ views/auth/login.php
@@ -5,6 +5,9 @@
  */
 
 header('Content-Type: text/html; charset=utf-8');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once '../../config/config.php';
 
 $error = '';
